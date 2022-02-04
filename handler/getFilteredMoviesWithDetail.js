0ff72e05ab38ad96ed8bdbd8598a147875efcc44
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-02-04 14:28:31 +0700

    flint-be: refactor movie detail and add error handling

--- handler/getFilteredMoviesWithDetail.js
+++ handler/getFilteredMoviesWithDetail.js
@@ -37,34 +37,41 @@ module.exports = async (req, res) => {
                 }
             }
         ])
-        console.log(movies)
+        // console.log(movies)
 
         const movies_detail = [];
         await Promise.all(
             movies.map(async (item) => {
-                // console.log(links)
-                await axios.get(`https://api.themoviedb.org/3/movie/${item.links[0]['tmdbId']}?api_key=ab54f18236e63dc6daf7c9aa35047444`)
-                    .then(response => {
-                        const { id, imdb_id, title, original_title, poster_path } = response.data;
-                        // console.log("data: ", data);
-                        const movie_detail = {
-                            id, imdb_id, title, original_title,
-                            "poster_path": `https://image.tmdb.org/t/p/w300${poster_path}`
-                        }
-                        movies_detail.push({ ...item, movie_detail })
-                    })
+                console.log(item.links[0])
+                const { movieId: movie_id, tmdbId: tmbdb_id } = item.links[0];
+                try {
+                    await axios.get(`https://api.themoviedb.org/3/movie/${item.links[0]['tmdbId']}?api_key=ab54f18236e63dc6daf7c9aa35047444`)
+                        .then(response => {
+                            // console.log("data: ", response.data);
+                            const { id, imdb_id, title, original_title, poster_path, overview } = response.data;
+                            // console.log("data: ", data);
+                            const movie_detail = {
+                                movie_id, tmbdb_id, imdb_id, title, original_title, overview,
+                                "poster_path": `https://image.tmdb.org/t/p/w300${poster_path}`
+                            }
+                            movies_detail.push({ ...item, movie_detail })
+                        })
+                } catch (error) {
+                    // console.log("di sini error: ", error)
+                }
                 // return movies_detail;
             })
         )
-        console.log(movies_detail)
+        // console.log(movies_detail)
         res.status(200).json({
             status: 'success',
             data: {
-                movies: movies_detail
+                movies: movies_detail,
+                item_count: movies_detail.length
             },
         })
     } catch (error) {
-        console.log(error)
+        // console.log(error)
         return res.status(500).send("Something is happened");
     }
 
